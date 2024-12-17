6f361aaabe9f33371a00a9d57e06687075901528
skrijwan <skrijwan329@gmail.com>
2024-12-17 19:21:01 +0530

    day 1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { TypeAnimation } from 'react-type-animation';
 
 export default function home({Username}) {
     const naviget = useNavigate()
@@ -9,15 +10,35 @@ export default function home({Username}) {
     }
     const handleclick =(e)=>{
         e.preventDefault()
+        if(person.personname===""){
+             return alert("why you test my coding skill ? bhai name enter kardo 𓆩😅𓆪")
+        }
+
         Username(person.personname)
         naviget("/wishpage")
     }
     return (
-        <div style={{display:"flex",height:"100vh",justifyContent:"center",alignItems:"center"}} >
-            <div className="form-box" style={{height:"300px",width:"330px",backgroundColor:"#02e6a9",display:"flex",justifyContent:"center",alignItems:"center",borderRadius:"10px"}}>
+        <div className='main-box' style={{display:"flex",height:"100vh",justifyContent:"center",alignItems:"center",flexDirection:"column",gap:"25px"}} >
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                'WELCOME !',
+                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                '2025',
+                1000
+              ]}
+              wrapper="b"
+              speed={40}
+              repeat={Infinity}
+              style={{ fontSize: "60px", color:"#ff4000" ,fontFamily:"Ubuntu"}}
+            />
+
+            
+        
+            <div className="form-box" style={{height:"300px",width:"330px",display:"flex",justifyContent:"center",alignItems:"center",borderRadius:"13px",boxShadow:"rgba(0, 0, 0, 0.75) 5px 8px 0px 0px",border:"1px solid #000000"}}>
                 <form style={{display:"flex",flexDirection:"column", gap:"28px", justifyContent:"center",alignItems:"center"}} >
-                    <input name='personname' onChange={onchange} type="text" placeholder='Enter your name' style={{padding:"7px",width:"280px",height:"20px",outline:"none",border:"none",borderRadius:"4px"}} value={setperson.personname} />
-                    <button onClick={handleclick} className='namebtn' type="submit" style={{padding:"5px",width:"160px",borderRadius:"10px",height:"40px",border:"none",outline:"none",backgroundColor:"#0025ff",color:"white",cursor:"pointer"}}>SUBMIT</button>
+                    <input name='personname' className='inputbox' onChange={onchange} type="text" placeholder='Enter your name' style={{padding:"7px",width:"280px",height:"30px",outline:"none",border:"1px solid #000000",borderRadius:"4px",background:"transparent"}} value={setperson.personname}  />
+                    <button onClick={handleclick}  className='namebtn' type="submit" style={{padding:"5px",width:"160px",borderRadius:"10px",height:"40px",border:"none",outline:"none",backgroundColor:"#0025ff",color:"white",cursor:"pointer"}}>SUBMIT</button>
                 </form>
 
             </div>
--- src/components/Wishpage.jsx
+++ src/components/Wishpage.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Wishpage({username}) {
   return (
     <div>
+    {username.name==="Rani Dey"||"rani"||"Bishaka dey"||"Bishaka"?"Hi HWC i know you are best frined of rijwan. happy new year to you":`Hi ${username.name} happy new year.`}
      <h1>{`Hi ${username.name} happy new year.`}</h1> 
       {console.log(username.name)}
     </div>
