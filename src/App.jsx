75196626753a266ab29288c7ca157757c1a74574
skrijwan <skrijwan329@gmail.com>
2024-12-20 11:54:10 +0530

    fox erroe

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './App.css'
-import Home from './components/home';
+import Home from './components/Home';
 import Wishpage from './components/Wishpage'
 import {
   BrowserRouter ,
