79a2cf5d6a64190f0cf035ad3f013875b6c96496
skrijwan <skrijwan329@gmail.com>
2024-12-31 15:23:14 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div style={{fontFamily:"monospace",fontSize:"15px"}}>
-      <p>&#169;Sk Rijwan.</p>
+      <p>&#169;BPC(BOT).</p>
     </div>
   )
 }
